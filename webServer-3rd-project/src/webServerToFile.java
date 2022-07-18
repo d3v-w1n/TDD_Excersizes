a939a0800eca0cdf410e10d0d9fa5dd6edeca431
los-xphp <carlosapplicant@gmail.com>
2022-07-18 12:59:37 -0400

    Debugging - File Path Created

--- webServer-3rd-project/src/webServerToFile.java
+++ webServer-3rd-project/src/webServerToFile.java
@@ -9,6 +9,7 @@ import java.time.temporal.Temporal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.nio.file.InvalidPathException;
 
@@ -56,21 +57,51 @@ public class webServerToFile extends Thread implements Serializable{
 		 */
 	}
 	
+	public static String ParseIP(String inputIP)
+	{
+	inputIP=inputIP.split(":")[1];
+	Matcher matcher = Pattern.compile("(:[^:]+:)(:[^:]+:)\\.([^.]+)").matcher(inputIP);
+	while (matcher.find()) {
+	     System.out.println(matcher.group().replaceAll("\\:", "/"));
+	}
+	/*Pattern pattern = Pattern.compile("\\..*?.|:|.*?:|");
+	Matcher matcher = pattern.matcher(inputIP);
+	while (matcher.find()) {
+	    System.out.println(matcher.group());
+	}
+	inputIP = matcher.group();
+	*/return inputIP;
+	}
+	
+	public static boolean ValidateIP(String input_IP)
+    {
+        String numRange = "(\\d{1,2}|(0|1)\\" + "d{2}|2[0-4]\\d|25[0-5])" + "\\." 
+              + "(\\d{1,2}|(0|1)\\" + "d{2}|2[0-4]\\d|25[0-5])" + "\\." 
+              + "(\\d{1,2}|(0|1)\\" + "d{2}|2[0-4]\\d|25[0-5])" + "\\." 
+              + "(\\d{1,2}|(0|1)\\" + "d{2}|2[0-4]\\d|25[0-5])";
+        
+        Pattern ip_pattern = Pattern.compile(numRange); 
+        Matcher match= ip_pattern.matcher(input_IP);
+        return match.matches();
+    }
+	
 	public final static Thread serverHandler(ServerSocket s) {
 		try {
 			newHandler(s);
-			String pathTree = Pattern.compile(s.getInetAddress().getHostAddress()).toString();
+			String userDir = System.getProperty("user.dir");
+			String path = Pattern.compile(s.getInetAddress().getHostAddress()).toString();
+			String pathTree = userDir + "/" + ParseIP(path).concat("/").concat(LocalDate.now().toString().concat(".txt"));
 			System.out.printf("%s%n", "Connection Path Tree - - - " + pathTree);
 			}catch(IOException exc) {
 		exc.printStackTrace();
 		}
-		System.out.printf("%s%n", "Bug Tester II");
+		System.out.printf("%s%n", "Closing Server Client Connection : Port 8080");
 		return null;
 	}
 	
 	public static void newHandler(ServerSocket s) throws IOException {
 		try {
-		System.out.printf("%s%n", "Bug Tester");
+		System.out.printf("%s%n", "Connect Client to Begin: (port 8080)");
 		Socket newSocket = new Socket();
 		while(newSocket.getInetAddress() == null) {
 			newSocket = s.accept(); System.out.println("Connection Established " + newSocket.toString());
@@ -84,22 +115,28 @@ public class webServerToFile extends Thread implements Serializable{
 public static void readOutServer(Socket s1) {	
 	try {
 	String pathDir = Pattern.compile(s1.getInetAddress().getHostAddress()).toString();
-	System.out.printf("%s%S%n", "Bug Tester IV ", pathDir);
-	String PathTree = new URL(pathDir).toString(); 
-	System.out.printf("%s%S%n", "Bug Tester V ", PathTree);
+	//System.out.printf("%s%S%n", "Bug Tester IV ", pathDir);
+	System.out.printf("%s%S%n", "VALID IP PATH REPRESENTATION: ", ValidateIP(pathDir));
+	System.out.printf("%s%S%n", "IP PARSED REPRESENTATION: ", ParseIP(pathDir));
+	//String PathTree = new URL(pathDir).toString(); 
+	//System.out.printf("%s%S%n", "Bug Tester V ", InetAddress.getByName(pathDir));
 	String fn = new File(pathDir).getName();
-	System.out.printf("%s%s%n", "Bug Tester VI ", fn);
-	String filename = PathTree.concat(LocalDate.now().toString().concat(".txt"));
-	System.out.printf("%s%S%n", "Bug Tester VII ", filename);
-	File directory = new File(PathTree);
-	//PrintWriter out = new PrintWriter(s1.getOutputStream(), true);
+	//System.out.printf("%s%s%n", "Bug Tester VI ", fn);
+	String filename = "/" + ParseIP(pathDir).concat("/").concat(LocalDate.now().toString());
+	System.out.printf("%s%S%n", "Local Directory + File Name: ", filename);
+	String workingDir = System.getProperty("user.dir");
+	File directory = new File(workingDir + filename);
 	if (!directory.exists()) directory.mkdirs();
 	Files.createDirectories(Paths.get(filename)).toFile(); //.toFile();
-	BufferedReader in = new BufferedReader(new InputStreamReader(s1.getInputStream()));
-	BufferedReader streamIn = new BufferedReader(new InputStreamReader(System.in));
-	FileOutputStream fos = new FileOutputStream(PathTree + filename);
+	directory.createNewFile();
+	String autotext = "New File Created From Simple Server Connection";
+	PrintWriter out = new PrintWriter(directory + ".txt", autotext);
+	out.print(autotext); out.close();
+	//BufferedReader in = new BufferedReader(new InputStreamReader(s1.getInputStream()));
+	//BufferedReader streamIn = new BufferedReader(new InputStreamReader(System.in));
+	FileOutputStream fos = new FileOutputStream(workingDir + filename);
 	ObjectOutputStream oos = new ObjectOutputStream(fos);
-	if(in != null) {
+	if(filename != null) {
 		String autogentext = "New File Created From Simple Server Connection";
 		Files.writeString(Paths.get(filename), autogentext);
 		//oos.writeFields();
