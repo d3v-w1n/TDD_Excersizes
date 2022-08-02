12b9878e979588d5393a77eb1241580247b31f1a
los-xphp <carlosapplicant@gmail.com>
2022-08-01 23:26:14 -0400

    1ST Commit - SpringBoot Intro

---
+++ UTC-HW4/src/UTCmain.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.time.temporal.*;
+
+public class UTCmain {
+	
+	public static void main(String[] args) throws IOException {}
+	
+}
---
+++ UTC-HW4/src/UTCpost.java
@@ -0,0 +1,29 @@
+import java.time.temporal.*;
+
+public class UTCpost {
+
+	public String utcInput;
+	public String utcOutput;
+	
+	public String getUtcInput() {
+		return utcInput;
+	}
+
+	public void setUtcInput(String utcInput) {
+		this.utcInput = utcInput;
+	}
+
+	public UTCpost() {
+		// TODO Auto-generated constructor stub
+		//temporal import or LocalTime method call
+	}
+
+	public String getUtcOutput() {
+		return utcOutput;
+	}
+
+	public void setUtcOutput(String utcOutput) {
+		this.utcOutput = utcOutput;
+	}
+
+}
